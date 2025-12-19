4d9997b6e990ac8fcc87bdb4cea3cbd8f1cdb47e
Vishva Trivedi <vishvabtrivedi@gmail.com>
2025-12-19 21:08:26 +0530

    added login/signup and loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import CategoryPage from "./pages/CategoryPage";
 import ProductPage from "./pages/ProductPage";
@@ -6,11 +7,28 @@ import WishlistPage from "./pages/WishlistPage";
 import AskValueVue from "./pages/AskValueVue";
 import AboutUs from "./pages/AboutUs";
 import SmartDeals from "./pages/SmartDeals";
-
-
-
+import LoginSignup from "./pages/LoginSignup";
+import NetworkLoader from "./components/NetworkLoader";
 
 export default function App() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  if (!isOnline) {
+    return <NetworkLoader />;
+  }
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -20,6 +38,7 @@ export default function App() {
       <Route path="/ask" element={<AskValueVue />} />
       <Route path="/about" element={<AboutUs />} />
       <Route path="/deals" element={<SmartDeals />} />
+      <Route path="/auth" element={<LoginSignup />} />
     </Routes>
   );
 }
